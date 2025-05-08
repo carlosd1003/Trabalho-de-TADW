1b922052576fa7c169ea1f70ed7823226d337b82
carlosd1003 <danielcarlosbr1227@gmail.com>
2025-05-08 14:27:06 -0300

    Fiz o teste do criarPokemon, mas n testei

---
+++ pokedex atualizada/tests/teste_criarPokemon.php
@@ -0,0 +1,10 @@
+<?php
+    require_once "../conexao.php";
+    require_once "../function.php";
+
+    $national = "152";
+    $nome = "pikaraio";
+    $gen = "1";
+
+    criarPokemon($conexao, $national, $nome, $gen);
+?>
