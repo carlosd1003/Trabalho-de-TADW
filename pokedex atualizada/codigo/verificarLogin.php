b0ab6d6520071f51757e1cae790a57a25774c3f4
JpOliveiraa14 <jpjoaopedro9621@gmail.com>
2025-08-18 13:49:50 -0300

    testei login para pagina home (funcionou)

--- pokedex atualizada/codigo/verificarLogin.php
+++ pokedex atualizada/codigo/verificarLogin.php
@@ -4,26 +4,26 @@
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
-    $sql = "SELECT * FROM tb_usuario WHERE email = '$email'";
+    $sql = "SELECT * FROM usuario WHERE email = '$email'";
 
     $resultado = mysqli_query($conexao, $sql);
 
     if (mysqli_num_rows($resultado) == 0) {
-        header("Location: index.php");
+        header("Location: index.html");
     }
     else {
         $linha = mysqli_fetch_array($resultado);
         $senha_banco = $linha['senha'];
-        $tipo = $linha['tipo'];
+        $Tipo = $linha['Tipo'];
 
         if (password_verify($senha, $senha_banco)) {
             session_start();
             $_SESSION['logado'] = 'sim';
-            $_SESSION['tipo'] = $tipo;
+            $_SESSION['Tipo'] = $Tipo;
             header("Location: home.php");
         }
         else {
-            header("Location: index.php");
+            header("Location: index.html");
         }
     }
 ?>
