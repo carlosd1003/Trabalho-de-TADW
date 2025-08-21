76ae70ae72bb11140b2f5b1c6d53e376c687ed39
JpOliveiraa14 <jpjoaopedro9621@gmail.com>
2025-08-21 16:37:34 -0300

    fiz o listar treinador e o deletar (funcionou)

--- pokedex atualizada/codigo/listar_treinador.php
+++ pokedex atualizada/codigo/listar_treinador.php
@@ -6,6 +6,57 @@
     <title>Document</title>
 </head>
 <body>
-    
+     <?php
+    require_once "conexao.php";
+    require_once "function.php";
+
+    $lista_treinador = listarTreinador($conexao);
+
+    if (count($lista_treinador) == 0) {
+        echo "Não existem clientes cadastrados";
+    } else {
+    ?>
+        <table border="1">
+            <tr>
+                <td>Id</td>
+                <td>Nome</td>
+                <td>Idade</td>
+                <td>Gênero</td>
+                <td>Cidade</td>
+                <td>Região</td>
+                <td>Time Atual</td>
+                <td>Data</td>
+                <td>Pokemons</td>
+                <td colspan="2">Ação</td>
+            </tr>
+        <?php
+        foreach ($lista_treinador as $treinador) {
+            $idtreinador = $treinador['idtreinador'];
+            $nome = $treinador['nome'];
+            $idade = $treinador['idade'];
+            $genero = $treinador['genero'];
+            $cidade = $treinador['cidade'];
+            $regiao = $treinador['regiao'];
+            $time_atual = $treinador['time_atual'];
+            $data_cadastro = $treinador['data_cadastro'];
+            $idpokemon = $treinador['idpokemon'];
+
+            echo "<tr>";
+            echo "<td>$idtreinador</td>";
+            echo "<td>$nome</td>";
+            echo "<td>$idade</td>";
+            echo "<td>$genero</td>";
+            echo "<td>$cidade</td>";
+            echo "<td>$regiao</td>";
+            echo "<td>$time_atual</td>";
+            echo "<td>$data_cadastro</td>";
+            echo "<td>$idpokemon</td>";
+            echo "<td><a href='deletar_treinador.php?id=$idtreinador'>Excluir</a></td>";
+            echo "<td><a href='formtreinador.php?id=$idtreinador'>Editar</a></td>";
+            echo "</tr>";
+        }
+    }
+        ?>
+        </table>
 </body>
 </html>
